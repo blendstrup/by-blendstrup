5cd55509fcc539cb7be47e2cf3a9ce9a76b9dbdd
blendstrup <36919652+blendstrup@users.noreply.github.com>
2026-04-19 10:17:24 +0200

    style: update max-width of footer container to 7xl

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,7 +1,7 @@
 export default function SiteFooter() {
 	return (
 		<footer className="bg-ink-surface px-12 py-12 lg:px-16 lg:py-16">
-			<div className="mx-auto max-w-screen-xl">
+			<div className="mx-auto max-w-7xl">
 				<p className="font-normal text-linen/80 text-sm">By Blendstrup</p>
 			</div>
 		</footer>
